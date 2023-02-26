7a7195e3145f7703e0f5900ae6889f03bf9a3dc0
Brian <wamwangimathenge@gmail.com>
2023-02-26 20:39:06 +0300

    20 Passed

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def self.actors_list
+        self.characters[0].actor.full_name
+    end
   
 end
